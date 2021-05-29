1fb4b4c06e23f5bb7c88a6b25116965e0ef0cd97
themanavshah <shahmanav765@gmail.com>
2021-05-29 19:11:04 +0530

    initiated with buffer

--- src/nodejs_ipfs/routes.js
+++ src/nodejs_ipfs/routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const fs = require('fs');
 const ipfsClient = require('ipfs-http-client').create;
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'uploads/' });
 
 const ipfs = ipfsClient({ host: 'localhost', port: '5001', protocol: 'http' });
 //console.log(ipfs);
@@ -20,21 +20,40 @@ router.get('/dashboard', (req, res) => {
 router.post('/upload', upload.single('file'), async (req, res) => {
     console.log(req.file)
     const file = req.file;
+    const pathx = file.path;
+    console.log(typeof pathx)
+    var filehash;
     const fileName = req.body.fileName;
-    const filePath = 'uploads/' + file.filename;
+    fs.readFile(pathx, 'utf8', function (err, data) {
+        // Display the file content
+        //console.log(data);
 
-    const fileHash = await addFile(fileName, filePath);
-    //console.log(fileHash);
-    fs.unlink(filePath, (err) => {
         if (err) {
-            console.log(err);
-        }
+            console.log(err)
+            return -1;
+        };
+        proccessit(data);
+
     });
-    res.json({ filename: fileName, fileHash: fileHash });
+    async function proccessit(file) {
+        var buf = Buffer.from(file);
+        console.log(buf);
+        filehash = await addFile(fileName, buf);
+    }
+
+
+    //console.log(fileHash);
+    // fs.unlink(filePath, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    // });
+    res.json({ filename: fileName, fileHash: filehash });
 });
 
-const addFile = async (fileName, filePath) => {
-    const file = fs.readFileSync(filePath);
+const addFile = async (fileName, file) => {
+    //var readable = await fs.createReadStream('file');
+    //console.log(readable);
     const fileAdded = await ipfs.add({ path: fileName, content: file })
     //console.log(fileAdded);
     const fileHash = fileAdded.cid.toString();
@@ -43,4 +62,4 @@ const addFile = async (fileName, filePath) => {
     return fileHash
 }
 
-module.exports = router
+module.exports = router 
